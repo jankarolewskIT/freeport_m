2d842d9a3804a2982b8c27058bccc78038ebd25f
“janeqqrul” <jan.karolewski1b@gmail.com>
2021-11-13 01:00:53 +0100

    add create_advice method to UserAdvices class for pulling advice form API.
    It checks if advices already exists in the set of advices previously created.

--- user/advices.py
+++ user/advices.py
@@ -1,3 +1,5 @@
+import requests
+
 from errors.validators import NotValidInput
 
 
@@ -15,3 +17,10 @@ class UserAdvices:
         if not 5 <= self.number <= 20:
             raise NotValidInput("Your input is not valid")
         return True
+
+    def create_advices(self, url: str) -> None:
+        prev_len = len(self.advices)
+        response = requests.request("GET", url).json().get("slip")
+        self.advices.add((response.get("id"), response.get("advice")))
+        if prev_len == len(self.advices):
+            self.create_advices(url)
